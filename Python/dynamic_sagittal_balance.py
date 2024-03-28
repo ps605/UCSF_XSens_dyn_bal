538521a2a70e06b18072cf213398cfa50bfd3e4a
Pavlos Silvestros <pavlossilv@gmail.com>
2024-03-27 17:52:17 -0700

    Update and check for mid shoulder position

--- Python/dynamic_sagittal_balance.py
+++ Python/dynamic_sagittal_balance.py
@@ -33,10 +33,10 @@ for csv_file in csv_files:
         data_xyz = pd.read_csv(trial_name + '_pos.csv')
         data_xyz = data_xyz.drop(columns='Frame')
 
-        # Get indeces of Pelvis and L/R Shoulders (Upper Arm segment) for position
-        idx_pelvis_p = int(data_xyz.columns.get_loc('Pelvis x'))
-        idx_shld_r_p = int(data_xyz.columns.get_loc('Right Upper Arm x'))
-        idx_shld_l_p = int(data_xyz.columns.get_loc('Left Upper Arm x')) 
+        # Get indeces of Pelvis and L/R Shoulders (Upper Arm segment) for position. Divide by three for split data
+        idx_pelvis_p = int(data_xyz.columns.get_loc('Pelvis x')/3)
+        idx_shld_r_p = int(data_xyz.columns.get_loc('Right Upper Arm x')/3)
+        idx_shld_l_p = int(data_xyz.columns.get_loc('Left Upper Arm x')/3) 
 
         # TO np.array and into mm
         pose_xyz = np.array(data_xyz, dtype='float')*1000
@@ -51,8 +51,8 @@ for csv_file in csv_files:
         data_q0123 = pd.read_csv(trial_name + '_qua.csv')  
         data_q0123 = data_q0123.drop(columns='Frame')      
 
-        # Get indeces of Pelvis and L/R Shoulders
-        idx_pelvis_q = int(data_q0123.columns.get_loc('Pelvis q0'))
+        # Get indeces of Pelvis and L/R Shoulders. Divide by 4 for split data
+        idx_pelvis_q = int(data_q0123.columns.get_loc('Pelvis q0')/4)
 
         # To np.array quaternion
         ori_quat = np.array(data_q0123, dtype='float')
@@ -109,7 +109,7 @@ for csv_file in csv_files:
         plt.xlabel('Anterior displacement (mm)')
         plt.ylabel('Lateral displacement (mm)') 
         plt.title(csv_file[0:-12])       
-        plt.savefig(data_path + 'Figures/d_MSPinP_' + csv_file[0:-12] + '.png')
+        plt.savefig(data_path + 'Figures/' + csv_file[0:-12] + '_d_MSPinP.png')
         plt.close()
 
         # Plot sagittal and coronal against frames
@@ -120,7 +120,7 @@ for csv_file in csv_files:
         plt.xlabel('Frame Number')
         plt.ylabel('Mid-Shoulder Position in Pelvis Reference System') 
         plt.legend(['Red - Sagittal (A/P)', 'Blue - Coronal (L/R)'], loc='upper right')
-        plt.savefig(data_path + 'Figures/d_MSPinP_sep_' + csv_file[0:-12] + '.png')
+        plt.savefig(data_path + 'Figures/' + csv_file[0:-12] + '_d_MSPinP_sep.png')
         plt.close()
 
-        print(data_path + 'Figures/d_MSPinP_' + csv_file[0:-12] + '.png saved')
+        print(data_path + 'Figures/' + csv_file[0:-12] + '_d_MSPinP.png saved')
